7099271a91d1c265c398b3180ef949ec01c4f2d9
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2024-10-31 14:19:19 +0100

    added How are you

--- script.js
+++ script.js
@@ -1,78 +1,70 @@
-
 var todoListElement = document.getElementById("todoList");
 
 const list = [];
 
 let item = {
-  description,
-  isDone
-}
+    description,
+    isDone,
+};
 
-function showList(){
+function showList() {
+    todoListElement.innerHTML = "";
+    for (let i = 0; i < list.length; i++) {
+        const listItem = document.createElement("li");
+        const deleteBtn = document.createElement("button");
+        const editBtn = document.createElement("button");
 
-  todoListElement.innerHTML = '';
-  for(let i = 0; i < list.length; i++){
-    
-    const listItem = document.createElement("li");
-    const deleteBtn = document.createElement("button");
-    const editBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.addEventListener("click", () => {
+            deleteItem(i);
+        });
 
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.addEventListener('click',() => {
-    deleteItem(i);
-    })
-
-    editBtn.textContent = 'Done';
-    editBtn.addEventListener('click',() => {
-    markAsDone(i);
-    })
+        editBtn.textContent = "Done";
+        editBtn.addEventListener("click", () => {
+            markAsDone(i);
+        });
 
         listItem.textContent = list[i].description;
 
-    if(list[i].isDone){
-      listItem.style.textDecoration = 'line-through';
-    }
+        if (list[i].isDone) {
+            listItem.style.textDecoration = "line-through";
+        }
 
-    listItem.appendChild(editBtn);
-    listItem.appendChild(deleteBtn);
-    todoListElement.appendChild(listItem);
-  }
+        listItem.appendChild(editBtn);
+        listItem.appendChild(deleteBtn);
+        todoListElement.appendChild(listItem);
+    }
 }
 
 function addToList() {
-  const textAreaValue = document.getElementById("textArea").value;
-  const alertDiv = document.getElementById("alert");
+    const textAreaValue = document.getElementById("textArea").value;
+    const alertDiv = document.getElementById("alert");
 
-  if(textAreaValue === ''){
-		alertDiv.style.display = 'block';
-  }
-  else{
-    alertDiv.style.display = 'none';
-    const item ={
-      isDone:false,
-      description: textAreaValue
+    if (textAreaValue === "") {
+        alertDiv.style.display = "block";
+    } else {
+        alertDiv.style.display = "none";
+        const item = {
+            isDone: false,
+            description: textAreaValue,
+        };
+
+        list.push(item);
+
+        showList();
+        document.getElementById("textArea").value = "";
     }
-    
-    list.push(item);
-   
-    showList(); 
-    document.getElementById("textArea").value = "";
-   }
 }
 
-function deleteItem(index){
-  list.splice(index, 1);
-  
-  showList();
+function deleteItem(index) {
+    list.splice(index, 1);
+
+    showList();
 }
 
-function markAsDone(index){
-  const selectedItem = list[index];
-  selectedItem.isDone = true;
-  
-  
-  showList();
+function markAsDone(index) {
+    const selectedItem = list[index];
+    selectedItem.isDone = true;
+
+    showList();
 }
-
-
-
