b4a87ce4154ca3ecba2f1392355ecd59247e61d7
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2024-11-01 07:31:21 +0100

    added confirmation

--- todo/Program.cs
+++ todo/Program.cs
@@ -3,4 +3,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("How are you?");
 Console.WriteLine("Fine!");
 Thread.Sleep(3000);
-Console.WriteLine("oh, godd, I was wondering!")
+Console.WriteLine("oh, good, I was wondering!")
+Console.WriteLine("Thank you for asking!")
